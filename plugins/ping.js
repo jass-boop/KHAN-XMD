631ccd5e9297e246df03494b6adb5073a9af00e9
Jawad Tech <jawadofcx@gmail.com>
2025-01-14 13:59:59 +0500

    Add files via upload

--- plugins/ping.js
+++ plugins/ping.js
@@ -21,7 +21,7 @@ async (conn, mek, m, { from, quoted, reply }) => {
 
         // Send the ping result
         await conn.sendMessage(from, { 
-            text: `*⚡ KHAN-AI SPEED: ${ping}ms*`, 
+            text: `*⚡ KHAN-MD SPEED: ${ping}ms*`, 
             contextInfo: {
                 mentionedJid: [m.sender],
                 forwardingScore: 999,
@@ -54,7 +54,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const message = await conn.sendMessage(from, { text: '> *PINGING...*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `> *🔥 KHAN-AI SPEED : ${ping}ms*` }, { quoted: message })
+        await conn.sendMessage(from, { text: `> *🔥 KHAN-MD SPEED : ${ping}ms*` }, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
--- plugins/updater.js
+++ plugins/updater.js
@@ -12,7 +12,7 @@ cmd({
     filename: __filename
 }, async (conn, mek, m, { from, reply }) => {
     try {
-        const repoUrl = 'https://github.com/JawadYTX/KHAN-AI.git'; 
+        const repoUrl = 'https://github.com/JawadYTX/KHAN-XMD.git'; 
         const targetFolder = 'plugins';
 
         
